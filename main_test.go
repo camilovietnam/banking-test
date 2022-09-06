f0e011f9e776a2f7af2a6a27751504ffb72eb1fc
Camilo <camilo.uran@nfon.com>
2022-09-06 12:30:49 +0200

    base solution

--- main_test.go
+++ main_test.go
@@ -1,55 +1,35 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"github.com/camilovietnam/banking-test/handlers"
-	"io"
-	"net/http"
+	"github.com/camilovietnam/banking-test/types"
 	"sync"
 	"testing"
 )
 
 var (
-	totalRequests = 100
-
-	baseURL    = "http://localhost:8080"
-	resetURL   = fmt.Sprintf("%s/account/reset", baseURL)
-	balanceURL = fmt.Sprintf("%s/account/balance", baseURL)
-	depositURL = fmt.Sprintf("%s/account/deposit/1", baseURL)
+	totalRequests = 500
 )
 
 func TestOneHundredDeposits(t *testing.T) {
-	res, err := http.Post(resetURL, "application/json", nil)
-
-	var wg sync.WaitGroup
-	wg.Add(totalRequests)
+	var (
+		account = types.NewAccount()
+		wg      sync.WaitGroup
+	)
 
 	for i := 0; i < totalRequests; i++ {
+		wg.Add(1)
+
 		go func() {
-			_, _ = http.Post(depositURL, "application/json", nil)
-			wg.Done()
+			defer wg.Done()
+			account.Deposit(1)
 		}()
 	}
 
 	wg.Wait()
 
-	res, err = http.Get(balanceURL)
-	if err != nil {
-		t.Fatal("http error: ", err)
-	}
+	balance := account.Balance()
 
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		t.Fatal("read error: ", err)
-	}
-
-	var response handlers.HttpResponse
-	if err = json.Unmarshal(body, &response); err != nil {
-		t.Fatal("could not unmarshal response", err)
-	}
-
-	if response.Balance != totalRequests {
-		t.Fatalf("Incorrect balance - Got: %d, Expected: %d", response.Balance, totalRequests)
+	if balance != totalRequests {
+		t.Fatalf("Incorrect balance - Got: %d, Expected: %d", balance, totalRequests)
 	}
 }
