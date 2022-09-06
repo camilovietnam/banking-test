dc3245abf2e43cd1d52f3dd9fe4c3b455b852ebf
Camilo <camilo.uran@nfon.com>
2022-09-06 11:16:34 +0200

    change task description

--- main.go
+++ main.go
@@ -4,20 +4,16 @@ package main
 // TODO: Implement the interface so that it can be used inside http handlers to perform account operations
 // TODO: Implement the endpoints provided in the main function
 type Accountable interface {
-	// Deposit adds to the balance of the account
 	Deposit(int)
-	// Withdraw removes from the balance of the account
 	Withdraw(int)
-	// Balance returns the current account balance
 	Balance() int
-	// Reset sets the account balance to zero
 	Reset()
 }
 
 func main() {
 	// TODO: implement the following endpoints. Use the HTTP verbs you consider adequate.
-	// /account/reset - Should set the account balance to zero
-	// /account/deposit/{amount} - should add the {amount} to the balance of the account
-	// /account/withdraw/{amount} - should remove the {amount} from the balance of the account
-	// /account/balance - should return the current account balance
+	// /account/reset
+	// /account/deposit/{amount}
+	// /account/withdraw/{amount}
+	// /account/balance
 }
